9415ae89717a0104125164dc90d1972593be647b
Aronas Zilys <zilysaronas@gmail.com>
2025-08-08 16:04:48 +0100

    Time: 992 ms (48.78%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+
+with flags as
+(
+select
+*,
+dense_rank() over(partition by departmentid order by salary desc) top_flag
+from employee
+)
+
+select
+d.name as Department,
+f.name as Employee,
+f.salary as Salary
+from flags f
+join department d on f.departmentid = d.id
+where top_flag < 4
